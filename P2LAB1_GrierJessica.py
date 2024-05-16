d15733e1fa1a09929c4c18b5f8d5fc2492780b06
grierJessica <160083581+grierJessica@users.noreply.github.com>
2024-05-15 20:39:31 -0400

    Add files via upload
    P2LAB1

---
+++ P2LAB1_GrierJessica.py
@@ -0,0 +1,18 @@
+# Jessica Grier 
+
+# 3/2/2024
+
+# P2LAB1
+
+# Mathematical calculations and display for users
+
+
+miles = float(input())
+gas = float(input())
+mpg = gas/miles
+
+your_value1= (mpg)* 20
+your_value2= (mpg)* 75
+your_value3= (mpg)* 500
+
+print(f'{your_value1:.2f} {your_value2:.2f} {your_value3:.2f}')
