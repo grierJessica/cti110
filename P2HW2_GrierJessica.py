ab8af3387e3bfae6477f9ed1a51cfee4cefe7d21
grierJessica <160083581+grierJessica@users.noreply.github.com>
2024-05-15 20:43:38 -0400

    Add files via upload
    P2HW2

---
+++ P2HW2_GrierJessica.py
@@ -0,0 +1,18 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/jessi/AppData/Local/Programs/Python/Python312/p2hw1_sample.py
+This program calculates and displays travel expenses
+Enter budget:1200
+Enter your travel destination:ASHVILLE
+How much do you think you will spend on gas?250
+Approximately, how much will you need for accommodation/hotel?300
+Last, how much do you need for food?200
+------------Travel Expenses------------
+Location:          ASHVILLE
+Initial Budget:    $1200.00
+Fuel:              $250.00
+Accomodation:      $300.00
+Food:              $200.00
+-----------------------------------------
+Remaining Balance: $450.00
