d3edcd581a44b7ece04ce86fdd013a1590a34b85
grierJessica <160083581+grierJessica@users.noreply.github.com>
2024-05-15 20:40:57 -0400

    Add files via upload
    P2LAB2

---
+++ P2LAB2_GrierJessica.py.py
@@ -0,0 +1,20 @@
+# Jessica Grier 
+
+# 3/2/2024
+
+# P1LAB2
+
+# Simple Statistics 
+
+
+
+num1 = float(input())
+num2 = float(input())
+num3 = float(input())
+num4 = float(input())
+
+avg = (num1 + num2 + num3 + num4) / 4.0
+product = (num1 * num2 * num3 * num4)
+    
+print('{:.0f}'.format(product), end = ' '), print('{:.0f}'.format(avg))
+print('{:.3f}'.format(product), end = ' '), print('{:.3f}'.format(avg))
